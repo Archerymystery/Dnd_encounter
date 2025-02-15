2e5359828f10bbf04d598c8412c330af58a8f710
11a28 <11a28@camp.rl.kiev.ua>
2025-02-15 11:41:35 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from config import Config
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
